df90b36152acfc4fa84f49f4bf9da8f4769d1d15
krypten <chaitiagrawal@gmail.com>
2017-04-07 11:50:22 +0530

    feat: Added 'MSE' Node to calculate the mean square error in neural network
    Added 'MSE' Node to calculate the mean square error in neural network. Mean squared error (MSE) is used to calculate the cost. This cost is used to define the accuracy of a neural network.
    
    Resolves #2

--- MiniFlow/__init__.py
+++ MiniFlow/__init__.py
@@ -2,6 +2,7 @@ from graph.Input import Input
 from graph.Add import Add
 from graph.Linear import Linear
 from graph.Sigmoid import Sigmoid
+from graph.MSE import MSE
 from graph.Graph import Graph
 
 import numpy as np
@@ -26,5 +27,26 @@ Output should be:
  [  1.23394576e-04   9.82013790e-01]]
 """
 print (output)
+
+print('------------------------------')
+
+print('------- MSE Output ----------')
+y, a = Input(), Input()
+cost = MSE(y, a)
+
+y_ = np.array([1, 2, 3])
+a_ = np.array([4.5, 5, 10])
+
+feed_dict = {y: y_, a: a_}
+graph = Graph.topological_sort(feed_dict)
+# forward pass
+Graph.forward_pass(cost, graph)
+
+"""
+Expected output
+
+23.4166666667
+"""
+print(cost.value)
 
 print('------------------------------')
